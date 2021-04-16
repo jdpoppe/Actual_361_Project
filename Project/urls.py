0a1b86b77f2f3e75bb3d69de85aeaeed110b143e
zetruxal <doming46@uwm.edu>
2021-04-16 17:03:28 -0500

    Revert "added templates folder and home.html"
    This reverts commit 20f9b79c

--- Project/urls.py
+++ Project/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from school_app.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Home.as_view())
 ]
