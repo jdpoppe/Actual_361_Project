dc4b0329a653b389cb4d1a6aa2b3535f1d89230d
zetruxal <doming46@uwm.edu>
2021-04-24 16:12:59 -0500

    added the login view

--- school_app/views.py
+++ school_app/views.py
@@ -29,3 +29,28 @@ class Assign(View):
             #Update lab to hold instructor
             return redirect("AssignTA")
 
+
+class Login(View):
+    def get(self, request):
+        return render(request, "home.html", {})
+
+    def post(self, request):
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = Employee.objects.get(EMP_EMAIL=request.POST['email'])
+            badPassword = (m.EMP_PASSWORD != request.POST['password'])
+        except:
+            noSuchUser = True
+        if noSuchUser or badPassword:
+            return render(request, "home.html", {"message": "Incorrect email/password"})
+        else:
+            request.session["email"] = m.EMP_EMAIL
+            return redirect("/dashboard/")
+
+
+class Dashboard(View):
+    def get(self, request):
+        m = request.session["email"]
+        return render(request, "dashboard.html", {"email": m})
+
