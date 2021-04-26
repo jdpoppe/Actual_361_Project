9dec03846a19ec8fce2c9967393b9b2b33d7abef
zetruxal <doming46@uwm.edu>
2021-04-26 13:37:15 -0500

    first draft of creating account created

--- school_app/admin.py
+++ school_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import MyUser, employee
+from .models import Employee
 
 # Register your models here.
-admin.site.register(MyUser)
-admin.site.register(employee)
+admin.site.register(Employee)
