3e2e665d14efb238bec98faa4a8165a181e1a4c4
John Poppe <jdpoppe@uwm.edu>
2021-04-27 03:02:01 -0500

    Final Updates

--- school_app/admin.py
+++ school_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, Course, Section
 
-# Register your models here.
-#admin.site.register(MyUser)
 admin.site.register(Employee)
+admin.site.register(Course)
+admin.site.register(Section)
