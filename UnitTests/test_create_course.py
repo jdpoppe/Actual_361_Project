dce07afdb880d7e742185b96738e29ded6aa31c4
John Poppe <jdpoppe@uwm.edu>
2021-04-26 20:36:23 -0500

    Even more tests

--- UnitTests/test_create_course.py
+++ UnitTests/test_create_course.py
@@ -5,8 +5,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 import django
 django.setup()
 
-from school_app.models import Employee, Course, Section
-from school_app.Helpers import createCourse, createSection, assignTA, assignInstructor
+from school_app.models import Employee, Course
+from school_app.Helpers import createCourse
 
 
 # Create your tests here.
@@ -14,16 +14,36 @@ from school_app.Helpers import createCourse, createSection, assignTA, assignInst
 class TestCreateCourse(unittest.TestCase):
     def setUp(self):
         self.instructor = Employee.objects.create(Email_Field="jimgaffigan@uwm.edu", EMP_FNAME="Jim",
-                                                  EMP_LNAME="Gaffigan", EMP_ROLE=1, EMP_INITIAL="T", EMP_PASSWORD="123")
+                                                  EMP_LNAME="Gaffigan", EMP_ROLE="Instructor", EMP_INITIAL="T",
+                                                  EMP_PASSWORD="123")
+        self.supervisor = Employee.objects.create(Email_Field="bertkreischer@uwm.edu", EMP_FNAME="Bert",
+                                                  EMP_LNAME="Kreischer", EMP_ROLE="Supervisor", EMP_INITIAL="A",
+                                                  EMP_PASSWORD="456")
         self.course = Course.objects.create(title="Comedy 251", instructor="jimgaffigan@uwm.edu")
     def test_noTitle(self):
         self.assertEqual("Course must have title", createCourse("", "jimgaffigan@uwm.edu"),
                          msg="Entering a course without a title fails to return message \"Course must have title\"")
 
     def test_courseExists(self):
-        self.assertEqual("Course already exists", createCourse("Comedy 251", "jimgaffigan@uwm.edu"))
+        self.assertEqual("Course already exists", createCourse("Comedy 251", "jimgaffigan@uwm.edu"),
+                         msg="Entering a course that already exists fails to return message "
+                             "\"Course already exists\"")
 
     def test_instructorDNE(self):
-        self.assertEqual("Instructor does not exist", createCourse("Comedy 361", "tomsegurra@uwm.edu"))
+        self.assertEqual("Instructor does not exist, or employee is not an Instructor",
+                         createCourse("Comedy 361", "tomsegurra@uwm.edu"),
+                         msg="Entering an instructor that does not exist fails to return message"
+                             "\"Instructor does not exist, or employee is not an Instructor\"")
 
-    def test_instruc
+    def test_employeeNotInstructor(self):
+        self.assertEqual("Instructor does not exist, or employee is not an Instructor",
+                         createCourse("Comedy 361", "bertkreischer@uwm.edu"),
+                         msg = "Entering an employee who is not an instructor does not return message, "
+                               "\"Instructor does not exist, or employee is not an Instructor\"")
+
+    def test_validInput(self):
+        self.assertEqual("Course successfully created", createCourse("Comedy 351","jimgaffigan@uwm.edu"),
+                         msg="Entering an untaken course title and instructor fails to return message "
+                             "\"Course successfully created\"")
+
+
---
+++ UnitTests/test_creeate_section.py
@@ -0,0 +1,56 @@
+import unittest
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+import django
+django.setup()
+
+from school_app.models import Employee, Course, Section
+from school_app.Helpers import createSection
+
+class createSection(unittest.TestCase):
+    def setUp(self):
+        self.instructor = Employee.objects.create(Email_Field="jimgaffigan@uwm.edu", EMP_FNAME="Jim",
+                                                  EMP_LNAME="Gaffigan", EMP_ROLE="Instructor", EMP_INITIAL="T",
+                                                  EMP_PASSWORD="123")
+        self.ta = Employee.objects.create(Email_Field="bertkreischer@uwm.edu", EMP_FNAME="Bert",
+                                                  EMP_LNAME="Kreischer", EMP_ROLE="TA", EMP_INITIAL="A",
+                                                  EMP_PASSWORD="456")
+        self.course = Course.objects.create(title="Comedy 251", instructor="jimgaffigan@uwm.edu")
+        self.section = Section.objects.create(title="Lab 801", course="Comedy 251", ta="bertkreischer@uwm.edu")
+
+    def test_noTitle(self):
+        self.assertEqual("Section needs title", createSection("", "bertkreischer@uwm.edu", "Comedy 251"),
+                         msg="Entering a section with no title fails to return message, \"Section needs title\"")
+
+    def test_noCourse(self):
+        self.assertEqual("Must enter course", createSection("Lab 802", "bertkreischer@uwm.edu", ""),
+                         msg="Entering a section with no parent course fails to return message, \"Must enter course\"")
+
+    def test_courseDNE(self):
+        self.assertEqual("Course does not exists", createSection("Lab 802", "bertkreischer@uwm.edu", "Comedy 361"),
+                         msg="Entering a section with a course that does not exist fails to return message, "
+                             "\"Course does not exists\"")
+
+    def test_taDNE(self):
+        self.assertEqual("TA does not exist, or employee is not TA",
+                         createSection("Lab 802", "tomsegurra@uwm.edu", "Comedy 251"),
+                         msg="Entering a TA that does not exist fails to return message "
+                             "\"TA does not exist, or employee is not TA\"")
+
+    def test_employeeNotTA(self):
+        self.assertEqual("TA does not exist, or employee is not TA",
+                         createSection("Lab 802", "jimgaffigan@uwm.edu", "Comedy 251"),
+                        msg="Entering an employee that is not a TA for a section fails to return message "
+                            "\"TA does not exist, or employee is not TA\"")
+
+    def test_sectionExists(self):
+        self.assertEqual("Section already exists", createSection("Lab 801", "bertkreischer@uwm.edu", "Comedy 251"),
+                         msg="Entering a section that already exists fails to return message, "
+                             "\"Section already exists\"")
+
+    def test_validInput(self):
+        self.assertEqual("Section successfully created",
+                         createSection("Lab 802", "bertkreischer@uwm.edu", "Comedy 251"),
+                         msg="Entering a valid ta, course, and title for a section fails to return message"
+                             "\"Section successfully created\"")
