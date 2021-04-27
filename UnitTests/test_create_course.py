ca7995ce5571f35508587e6676e21581f013a634
John Poppe <jdpoppe@uwm.edu>
2021-04-26 19:52:26 -0500

    More tests

--- UnitTests/test_create_course.py
+++ UnitTests/test_create_course.py
@@ -15,8 +15,15 @@ class TestCreateCourse(unittest.TestCase):
     def setUp(self):
         self.instructor = Employee.objects.create(Email_Field="jimgaffigan@uwm.edu", EMP_FNAME="Jim",
                                                   EMP_LNAME="Gaffigan", EMP_ROLE=1, EMP_INITIAL="T", EMP_PASSWORD="123")
-
+        self.course = Course.objects.create(title="Comedy 251", instructor="jimgaffigan@uwm.edu")
     def test_noTitle(self):
         self.assertEqual("Course must have title", createCourse("", "jimgaffigan@uwm.edu"),
                          msg="Entering a course without a title fails to return message \"Course must have title\"")
 
+    def test_courseExists(self):
+        self.assertEqual("Course already exists", createCourse("Comedy 251", "jimgaffigan@uwm.edu"))
+
+    def test_instructorDNE(self):
+        self.assertEqual("Instructor does not exist", createCourse("Comedy 361", "tomsegurra@uwm.edu"))
+
+    def test_instruc
